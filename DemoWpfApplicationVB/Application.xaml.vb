20247642651ef2dfc7f52637f3626fea0d33cfe1
raviraj.sonetta <raviraj.sonetta@BRD-PG00Q1WZ-L>
2018-04-05 11:58:42 +0530

    Test

--- DemoWpfApplicationVB/Application.xaml.vb
+++ DemoWpfApplicationVB/Application.xaml.vb
@@ -3,4 +3,5 @@
   ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
   ' can be handled in this file.
   'NFPCNG007: Feature implemented
+  'NFPCNG007: Test Commit
 End Class
